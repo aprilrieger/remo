420515d1338a63700e3533562ad332065a3790a9
April Rieger <63515648+aprilrieger@users.noreply.github.com>
2021-03-13 23:23:08 -0800

    Updated the Users column to include additional account information and configuring the devise gem, added the views, and configured the routes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  def index
+    users = User.all
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def destroy
+    User.destroy(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    render json: user
+  end
+
+  # private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :city, :state, :zip_code, :latitude, :longitude, :email, :password)
+  end
+end
